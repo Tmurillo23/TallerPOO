951a054319139eb16e1f20d6724aceb834dc1138
Tmurillo23 <tatianamurillomosqeura1@gmail.com>
2023-08-08 11:34:48 -0500

    Inicio del ejercicio 11

---
+++ taller_poo/cuenta_bancaria.py
@@ -0,0 +1,24 @@
+class CuentaBancaria:
+
+    def __init__(self, numero: str, titular: str):  # Este metodo inicializa los objetos de la clase y sus atributos
+        self.numero_de_cuenta: str = numero
+        self.saldo: float = 0.0
+        self.titular: str = titular
+
+    def depositar(self, cantidad: float):
+        self.saldo += cantidad
+
+    def retirar(self, cantidad: float) -> float:
+        if self.saldo >= cantidad:
+            self.saldo -= cantidad
+        else:
+            cantidad = self.saldo
+            self.saldo = 0.0
+        return cantidad
+
+    def aplicar_cuota_de_manejo(self):
+        cuota_de_manejo = (self.saldo) - (self.saldo*0.02)
+        return cuota_de_manejo
+    def mostrar_detalle(self, cuota_de_manejo):
+
+
